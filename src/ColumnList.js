a959a3f3b89ba2b8679ca8693b447440f1e8ab60
Aya Elsisy <A.Elsisy@nu.edu.eg>
2022-01-17 17:05:30 +0200

    construct column list

---
+++ src/ColumnList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import DataSourceItem from './DataSourceItem'
+
+function ColumnList (props)  {
+
+   
+        return (
+        <div className='column'>           
+        <h1>Data sources</h1>
+        <ul className='datasource' >
+          {props.datasources.map((datasource) => <DataSourceItem key = {datasource.name} name= {datasource.name} function= {datasource.function}{...datasource} ></DataSourceItem>)}
+        </ul>
+      
+      
+      </div>
+
+
+                        
+        )
+    }
+
+export default ColumnList
