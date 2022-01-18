ecb2b61fdc0266ee6fea7be1f1c7d0c4bf95db5d
Aya Elsisy <A.Elsisy@nu.edu.eg>
2022-01-18 03:48:11 +0200

    add: a stamp to each ditsinct type

--- src/components/DataSourceItem.js
+++ src/components/DataSourceItem.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import { useDrag } from 'react-dnd';
+import React from "react";
+import { useDrag } from "react-dnd";
 
+const DATASOURCEITEM = "DataSourceItem";
 
-const DATASOURCEITEM = 'DataSourceItem';
+function DataSourceItem(props) {
+  const [, drag] = useDrag({
+    type: props.item.function,
+    item: () => props.item,
+  });
 
-function DataSourceItem (props) {
-    
-    console.log("this is naaaameeee",props.item)
+  return (
+    <li
+      key={props.item.name}
+      className="datasource"
+      id={props.item?.function === "measure" ? "dimension" : "measure"}
+      ref={drag}
+    >
+      <p> {props.item.name} </p>
+    </li>
+  );
+}
 
-    const [, drag] = useDrag({
-        type: props.item.function,
-            item: () => (props.item)
-      });
-
-        return (
-            <li key={props.item.name} className="datasource" ref={drag} >
-
-
-             <p> {props.item.name} </p>
-            </li>
-          );
-        }
-
-
-export default DataSourceItem
+export default DataSourceItem;
