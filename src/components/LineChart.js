91bbb696932c823eff101c2a15cd810e36e56d07
Aya Elsisy <A.Elsisy@nu.edu.eg>
2022-01-18 03:48:31 +0200

    remove: unneded prints

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,54 +1,52 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import * as DataAPI from "../DataAPI.js";
+import * as DataAPI from "../APIS/DataAPI.js";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import { Chart } from "react-chartjs-2";
 
 function LineChart(props) {
-  const [res, setRes] = useState([]);
   const [xaxis, setxaxis] = useState([]);
   const [yaxis, setyaxis] = useState([]);
 
   useEffect(() => {
-    console.log("I AMMMM INNNN LINEEEEE CHARTTTTT");
-    console.log("thisss iss proooooops ", props.measures, props.dimension);
-    DataAPI.getData({
-      measures: props.measures,
-      dimension: props.dimension,
-    }).then((res) => {
-      setRes(res);
-      setxaxis(res[0]["values"]);
-      setyaxis(res[1]["values"]);
-      console.log("RESULLLLLLLLLLLLT:::: ", res);
-    });
-    console.log(xaxis, yaxis, "hereeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-  }, [props.measures]);
+    if (props.dimension && props.measures.length) {
+      DataAPI.getData({
+        measures: props.measures,
+        dimension: props.dimension,
+      }).then((res) => {
+        setxaxis(res[0]);
+        setyaxis(res.slice(1));
+      });
+    }
+  }, [props.measures, props.dimension]);
 
   const graphdata = {
-    labels: xaxis,
-    datasets: [
-      {
-        label: ["name name"],
-        backgroundColor: "rgba(75,192,192,1)",
-        borderColor: "rgba(0,0,0,1)",
-        borderWidth: 2,
-        data: yaxis,
-      },
-    ],
+    labels: xaxis.values,
+    datasets: yaxis.map((y) => ({
+      label: y.name,
+      backgroundColor: "rgba(75,192,192,1)", // make this random
+      borderColor: "rgba(0,0,0,1)", // make this random
+      borderWidth: 2,
+      data: y.values,
+    })),
   };
+  
   const options = {
     scales: {
-      yAxes: [
+      xAxes: [
         {
-          ticks: {
-            beginAtZero: true,
+          scaleLabel: {
+            display: true,
+            fontColor: "red",
+            fontSize: 55,
+            labelString: xaxis.name,
           },
         },
       ],
     },
   };
 
-  return res && <Line data={graphdata} options={options} />;
+  return xaxis && yaxis && <Line data={graphdata} options={options} />;
 }
 export default LineChart;
